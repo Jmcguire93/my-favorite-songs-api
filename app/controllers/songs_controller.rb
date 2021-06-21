e8af974b5a48eac820cfc1caecb5573040713f9a
James <james.ryan.mcguire@gmail.com>
2021-06-21 13:37:55 -0700

    Adds show route path and controller method

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,10 @@ class SongsController < ApplicationController
     songs = Song.all 
     render json: songs.as_json
   end
+
+  def show 
+    song_id = params[:id]
+    song = Song.find_by(id: song_id) 
+    render json: song.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   get "/songs", controller: "songs", action: "index"
 
+  get "/songs/:id", controller: "songs", action: "show"
 
 end
